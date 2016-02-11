f3edccd962250a06d6400384702c1884e7bfc319
Patrick Mays <Blueroofgames@gmail.com>
2016-02-11 18:59:51 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -1,1 +1,16 @@
 // State: Dodgy.play
+
+"use strict";
+
+var DogeDodge = {};
+DogeDodge.Play = function () {};
+DodgeDSodge.Play.prototype = {
+  init function () {
+    console.log("%c~~~Booting the Doge ~~~\n Compliments of Skilstak");
+    
+};
+
+
+
+
+
