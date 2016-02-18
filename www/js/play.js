ee00df1b000b58b484124535d3a3870ac8688be7
Patrick Mays <Blueroofgames@gmail.com>
2016-02-18 18:46:52 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -7,10 +7,22 @@ DogeDodge.Play = function () {};
 DodgeDSodge.Play.prototype = {
   init function () {
     console.log("%c~~~Booting the Doge ~~~\n Compliments of Skilstak");
-    
+
+  },
+  preload: function () {
+    this.load.image('background','assets/background.png');
+  },
+  
+  create: function () {
+    this.background = this.add.titleSprite(0,0,320,568,'background)';
+    //
+    //
+  },
+
+  update function () {
+  }
+
 };
-
-
 
 
 
