59c783ca5157df7aae6c146de888c1939dde20a0
Patrick Mays <Blueroofgames@gmail.com>
2016-02-25 18:36:45 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -17,9 +17,10 @@ DogeDodge.Play.prototype = {
   
   create: function () {
     this.background = this.add.tileSprite(0,0,320,568,'background');
-    this.background.autoScroll(0,50);
+    // this.background.autoScroll(0,50);
     this.background.scale.set(1);  
-  }
+  }, 
+
   update: function () {
   }
 };
