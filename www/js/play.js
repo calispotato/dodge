ff7469f7adb2539a26e3bb431b5c3366e3cd8a0b
Patrick Mays <Blueroofgames@gmail.com>
2016-02-26 14:12:05 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -13,15 +13,31 @@ DogeDodge.Play.prototype = {
   },
   preload: function () {
     this.load.image('background','assets/background.png');
+    this.load.spritesheet('dodger','assets/dodger.png',46,64,2);
   },
   
   create: function () {
     this.background = this.add.tileSprite(0,0,320,568,'background');
     // this.background.autoScroll(0,50);
     this.background.scale.set(1);  
+
+    // dodger 
+    this.dodger = this.add.sprite(160,510,'dodger');
+    this.dodger.anchor.set(0.5,0.5);
+    this.dodger.animations.add('blink');
+    this.dodger.animations.play('blink',2,true); // (name,fps,loop)
+
+    this.cursors = game.input.keyboard.createCursorKeys();
   }, 
 
   update: function () {
+    if (this.cursors.left.isDown) {
+      this.dodger.x -= 10;
+    }
+  if (this.cursors.right.isDown) {
+    this.dodger.x += 10;
+    }
+
   }
 };
 
