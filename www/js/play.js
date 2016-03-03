949d8649eb738536bd4674b5bba960385c8586d8
Patrick Mays <Blueroofgames@gmail.com>
2016-03-03 18:31:06 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -31,8 +31,6 @@ DogeDodge.Play.prototype = {
     this.cursors = game.input.keyboard.createCursorKeys();
 
     this.projectile = this.add.sprite(0, 0, 40, 40, 'projectile');
-    this.projectile.autoScroll(0, 100);
-    this.projectile.scale.set(1);
   }, 
 
   update: function () {
