09d2159483e4a3e5968b98efe6a911ecfa6c55cf
Patrick Mays <blueroofgames@gmail.com>
2016-04-07 18:07:30 -0400

    Update play.js

--- www/js/play.js
+++ www/js/play.js
@@ -52,11 +52,11 @@ DogeDodge.Play.prototype = {
   if (this.projectile.y >586) {
     this.projectile.y = -10
     this.projectile.x = game.rnd.integerInRange(1,320);
-    
+    }
   
   }
   
-};
+//};
 
 
 
