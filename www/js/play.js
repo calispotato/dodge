fc56299ad22e752bab4129ad4b8a9ed0183991be
Patrick Mays <blueroofgames@gmail.com>
2016-04-07 18:29:45 -0400

    Update play.js

--- www/js/play.js
+++ www/js/play.js
@@ -58,10 +58,10 @@ DogeDodge.Play.prototype = {
 
   handleCollision: function() {
     console.log("OUUCHH");
-    game.state.start('Play')
+   /* game.state.start('Play')
     }
   
-/*
+
   }
 };
 */
